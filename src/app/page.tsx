deb98593311790cc2f2e66189f9490c6a7203fd3
Marcos Vinicius <marcos.v.silva@live.com>
2023-07-26 23:47:34 +0100

    [feat] Create a wrapper page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,19 @@ import { ProductsList } from '@/components/products-list';
 import { FilterBar } from '../components/filter-bar';
 import styles from './page.module.css';
 
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <FilterBar />
-      <ProductsList />
+      <PageWrapper>
+        <FilterBar />
+        <ProductsList />
+      </PageWrapper>
     </main>
   );
 }
